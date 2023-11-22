089c818168ed3185f9b2122d5fe51cad1df20089
yaorangtayi <1033720276@qq.com>
2023-11-23 02:14:19 +0800

    Grid Bugfix

--- src/tools/schema.tsx
+++ src/tools/schema.tsx
@@ -1,8 +1,10 @@
 import { ISchema, RecursionField, SchemaKey } from '@formily/react';
+import { judgeIsEmpty } from '@yimoko/store';
 // TODO 待迁移至 @yimoko/store
 // 在很多场景组件对子组件有侵入性 必须放在顶层,需要将 schema 的配置转为子组件的 props
+// eslint-disable-next-line complexity
 export const ItemSchemaToProps = (schema: ISchema, name: string, RecursionProps?: Record<any, any>, schemaKey?: SchemaKey) => {
-  const { 'x-component': component, 'x-component-props': componentProps, 'x-decorator': decorator, 'x-decorator-props': decoratorProps } = schema ?? {};
+  const { 'x-component': component, 'x-component-props': componentProps, 'x-decorator': decorator, 'x-decorator-props': decoratorProps, properties } = schema ?? {};
   let itemProps: Record<any, any> = {};
   let curSchemaItem = schema;
   let onlyRenderProperties = false;
@@ -11,10 +13,14 @@ export const ItemSchemaToProps = (schema: ISchema, name: string, RecursionProps?
     curSchemaItem = { ...schema, type: 'void' };
     onlyRenderProperties = true;
     itemProps = { ...componentProps };
+    if (judgeIsEmpty(properties)) {
+      return itemProps;
+    }
   } else if (decorator === name) {
     curSchemaItem = { ...schema, 'x-decorator': '' };
     itemProps = { ...decoratorProps };
   }
+
   return {
     ...itemProps,
     children: (
